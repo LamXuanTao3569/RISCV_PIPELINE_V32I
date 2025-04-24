996cf6e18f6368e77b1067726d07c6a3583f7557
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-04-25 06:44:14 +0700

    update

--- pipelined/wb_stage.v
+++
@@ -1,8 +0,0 @@
-module wb_stage (
-    input clk, reset, stall,
-    input [31:0] mem_wb_result_in,
-    input [4:0] mem_wb_rd_in,
-    input mem_wb_reg_write_in
-);
-    // Không có logic hoặc output thêm
-endmodule
