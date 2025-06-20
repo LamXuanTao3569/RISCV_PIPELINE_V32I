b72759b4d57ffb1e151418fa7a2123f904786fa9
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-20 22:39:54 +0700

    update

--- pipeline_cache/src/Data_Memory.v
+++ pipeline_cache/src/Data_Memory.v
@@ -2,15 +2,14 @@ module Data_Memory(
     input clk, rst, WE,
     input [1:0] MemOp,
     input [31:0] A, WD,
-    output reg [31:0] RD,
-    output [31:0] memory_out [0:1023]
+    output reg [31:0] RD
 );
-    reg [31:0] mem [0:1023];
+    reg [31:0] memory [0:1023];
     integer i;
 
     initial begin
         for (i = 0; i < 1024; i = i + 1)
-            mem[i] = 32'h0;
+            memory[i] = 32'h0;
     end
 
     always @(posedge clk) begin
@@ -18,20 +17,20 @@ module Data_Memory(
             case (MemOp)
                 2'b00: begin // sb
                     case (A[1:0])
-                        2'b00: mem[A >> 2][7:0] <= WD[7:0];
-                        2'b01: mem[A >> 2][15:8] <= WD[7:0];
-                        2'b10: mem[A >> 2][23:16] <= WD[7:0];
-                        2'b11: mem[A >> 2][31:24] <= WD[7:0];
+                        2'b00: memory[A >> 2][7:0] <= WD[7:0];
+                        2'b01: memory[A >> 2][15:8] <= WD[7:0];
+                        2'b10: memory[A >> 2][23:16] <= WD[7:0];
+                        2'b11: memory[A >> 2][31:24] <= WD[7:0];
                     endcase
                 end
                 2'b01: begin // sh
                     case (A[1])
-                        1'b0: mem[A >> 2][15:0] <= WD[15:0];
-                        1'b1: mem[A >> 2][31:16] <= WD[15:0];
+                        1'b0: memory[A >> 2][15:0] <= WD[15:0];
+                        1'b1: memory[A >> 2][31:16] <= WD[15:0];
                     endcase
                 end
-                2'b10: mem[A >> 2] <= WD; // sw
-                default: mem[A >> 2] <= WD;
+                2'b10: memory[A >> 2] <= WD; // sw
+                default: memory[A >> 2] <= WD;
             endcase
         end
     end
@@ -43,36 +42,29 @@ module Data_Memory(
             case (MemOp)
                 2'b00: begin // lb
                     case (A[1:0])
-                        2'b00: RD = {{24{mem[A >> 2][7]}}, mem[A >> 2][7:0]};
-                        2'b01: RD = {{24{mem[A >> 2][15]}}, mem[A >> 2][15:8]};
-                        2'b10: RD = {{24{mem[A >> 2][23]}}, mem[A >> 2][23:16]};
-                        2'b11: RD = {{24{mem[A >> 2][31]}}, mem[A >> 2][31:24]};
+                        2'b00: RD = {{24{memory[A >> 2][7]}}, memory[A >> 2][7:0]};
+                        2'b01: RD = {{24{memory[A >> 2][15]}}, memory[A >> 2][15:8]};
+                        2'b10: RD = {{24{memory[A >> 2][23]}}, memory[A >> 2][23:16]};
+                        2'b11: RD = {{24{memory[A >> 2][31]}}, memory[A >> 2][31:24]};
                     endcase
                 end
                 2'b01: begin // lh
                     case (A[1])
-                        1'b0: RD = {{16{mem[A >> 2][15]}}, mem[A >> 2][15:0]};
-                        1'b1: RD = {{16{mem[A >> 2][31]}}, mem[A >> 2][31:16]};
+                        1'b0: RD = {{16{memory[A >> 2][15]}}, memory[A >> 2][15:0]};
+                        1'b1: RD = {{16{memory[A >> 2][31]}}, memory[A >> 2][31:16]};
                     endcase
                 end
-                2'b10: RD = mem[A >> 2]; // lw
+                2'b10: RD = memory[A >> 2]; // lw
                 2'b11: begin // lbu/lhu
                     case (A[1:0])
-                        2'b00: RD = {24'b0, mem[A >> 2][7:0]};
-                        2'b01: RD = {24'b0, mem[A >> 2][15:8]};
-                        2'b10: RD = {24'b0, mem[A >> 2][23:16]};
-                        2'b11: RD = {24'b0, mem[A >> 2][31:24]};
+                        2'b00: RD = {24'b0, memory[A >> 2][7:0]};
+                        2'b01: RD = {24'b0, memory[A >> 2][15:8]};
+                        2'b10: RD = {24'b0, memory[A >> 2][23:16]};
+                        2'b11: RD = {24'b0, memory[A >> 2][31:24]};
                     endcase
                 end
-                default: RD = mem[A >> 2];
+                default: RD = memory[A >> 2];
             endcase
         end
     end
-
-    genvar idx;
-    generate
-        for (idx = 0; idx < 1024; idx = idx + 1) begin : expose_mem
-            assign memory_out[idx] = mem[idx];
-        end
-    endgenerate
 endmodule
--- pipeline_cache/src/Instruction_Memory.v
+++ pipeline_cache/src/Instruction_Memory.v
@@ -1,22 +1,14 @@
-module Instruction_Memory(rst, A, RD, memory_out);
+module Instruction_Memory(rst, A, RD);
     input rst;
     input [31:0] A;
     output [31:0] RD;
-    output [31:0] memory_out [0:1023];
 
-    reg [31:0] mem [1023:0];
+    reg [31:0] memory [0:1023];
   
-    assign RD = (rst == 1'b0) ? {32{1'b0}} : mem[A[31:2]];
-
-    genvar idx;
-    generate
-        for (idx = 0; idx < 1024; idx = idx + 1) begin : expose_mem
-            assign memory_out[idx] = mem[idx];
-        end
-    endgenerate
+    assign RD = (rst == 1'b0) ? {32{1'b0}} : memory[A[31:2]];
 
     initial begin
         // Nạp từ file bên ngoài (cho simulation)
-        $readmemh("instructions.mem", mem);
+        $readmemh("instructions.mem", memory);
     end
 endmodule
