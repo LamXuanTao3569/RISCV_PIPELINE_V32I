ebe73a58df44acea605505d8e9b6102662be09d9
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-20 23:40:16 +0700

    update

--- pipeline_cache/src/Data_Memory.v
+++ pipeline_cache/src/Data_Memory.v
@@ -14,6 +14,7 @@ module Data_Memory(
 
     always @(posedge clk) begin
         if (WE) begin
+            $display("STORE: Addr=%h Data=%h MemOp=%b Time=%0t", A, WD, MemOp, $time);
             case (MemOp)
                 2'b00: begin // sb
                     case (A[1:0])
