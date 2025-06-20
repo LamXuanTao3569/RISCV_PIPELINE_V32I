a1ad53a8d80958798aaab474b50e21e7b70f3700
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-20 22:47:53 +0700

    update

--- pipeline_cache/src/Register_File.v
+++ pipeline_cache/src/Register_File.v
@@ -4,23 +4,23 @@ module Register_File(
     input [31:0] WD3,
     output [31:0] RD1, RD2
 );
-    reg [31:0] Register [31:0];
+    reg [31:0] registers [0:31];
     integer i;
 
     always @(posedge clk or negedge rst) begin
         if (rst == 1'b0) begin
             for (i = 0; i < 32; i = i + 1)
-                Register[i] <= 32'h00000000;
+                registers[i] <= 32'h00000000;
         end
         else begin
-            Register[0] <= 32'h0; // x0 is always 0
+            registers[0] <= 32'h0; // x0 is always 0
             if (WE3 && (A3 != 5'h00)) begin
-                Register[A3] <= WD3;
+                registers[A3] <= WD3;
             end
         end
     end
 
     // Asynchronous read
-    assign RD1 = (A1 == 5'h00) ? 32'h0 : Register[A1];
-    assign RD2 = (A2 == 5'h00) ? 32'h0 : Register[A2];
+    assign RD1 = (A1 == 5'h00) ? 32'h0 : registers[A1];
+    assign RD2 = (A2 == 5'h00) ? 32'h0 : registers[A2];
 endmodule
