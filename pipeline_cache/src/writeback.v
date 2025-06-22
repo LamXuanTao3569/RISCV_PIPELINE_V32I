7bf1cc89c8496e40a008da89e20469df508988a8
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-23 06:46:24 +0700

    update

--- pipeline_cache/src/writeback.v
+++ pipeline_cache/src/writeback.v
@@ -1,5 +1,4 @@
 module writeback(
-    input clk, rst_n,
     // from MEM/WB reg
     input RegWrite_in,
     input [1:0] ResultSrc_in,
@@ -9,24 +8,19 @@ module writeback(
     input [31:0] PCPlus4_in,
 
     // to Decode stage for register file write
-    output reg [31:0] result_wb,
+    output [31:0] result_wb,
 
     // to forwarding unit and register file
-    output reg RegWrite_wb,
-    output reg [4:0] rd_wb
+    output RegWrite_wb,
+    output [4:0] rd_wb
 );
-    always @(*) begin
-        case (ResultSrc_in)
-            2'b00: result_wb = ALU_Result_in;
-            2'b01: result_wb = ReadData_in;
-            2'b10: result_wb = PCPlus4_in; // For JAL
-            default: result_wb = ALU_Result_in; // Should not happen
-        endcase
-    end
-
     // Pass through control signal and destination register
-    always @(*) begin
-        RegWrite_wb = RegWrite_in;
-        rd_wb = rd_in;
-    end
+    assign RegWrite_wb = RegWrite_in;
+    assign rd_wb = rd_in;
+
+    // Mux for the result to be written back
+    assign result_wb = (ResultSrc_in == 2'b10) ? PCPlus4_in :
+                       (ResultSrc_in == 2'b01) ? ReadData_in :
+                       ALU_Result_in; // Default to ALU result
+
 endmodule 
