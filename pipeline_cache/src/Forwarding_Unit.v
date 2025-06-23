abf2d821b3aff2809a85d3bb0e911bd993410ef2
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-23 07:54:22 +0700

    update

--- pipeline_cache/src/Forwarding_Unit.v
+++ pipeline_cache/src/Forwarding_Unit.v
@@ -12,22 +12,22 @@ module Forwarding_Unit(
 
     always @(*) begin
         // Forwarding for ALU operand A
-        if (EX_MEM_RegWrite && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs1)) begin
-            ForwardA = 2'b01; // Forward from EX/MEM
-        end else if (MEM_WB_RegWrite && (MEM_WB_rd != 5'b0) && (MEM_WB_rd == ID_EX_rs1)) begin
-            ForwardA = 2'b10; // Forward from MEM/WB
-        end else begin
-            ForwardA = 2'b00; // No forwarding
-        end
+        if (EX_MEM_RegWrite && EX_MEM_rd != 0 && EX_MEM_rd == ID_EX_rs1)
+            ForwardA = 2'b01;
+        else if (MEM_WB_RegWrite && MEM_WB_rd != 0 && MEM_WB_rd == ID_EX_rs1
+                && ~(EX_MEM_RegWrite && EX_MEM_rd != 0 && EX_MEM_rd == ID_EX_rs1))
+            ForwardA = 2'b10;
+        else
+            ForwardA = 2'b00;
 
         // Forwarding for ALU operand B
-        if (EX_MEM_RegWrite && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs2)) begin
-            ForwardB = 2'b01; // Forward from EX/MEM
-        end else if (MEM_WB_RegWrite && (MEM_WB_rd != 5'b0) && (MEM_WB_rd == ID_EX_rs2)) begin
-            ForwardB = 2'b10; // Forward from MEM/WB
-        end else begin
-            ForwardB = 2'b00; // No forwarding
-        end
+        if (EX_MEM_RegWrite && EX_MEM_rd != 0 && EX_MEM_rd == ID_EX_rs2)
+            ForwardB = 2'b01;
+        else if (MEM_WB_RegWrite && MEM_WB_rd != 0 && MEM_WB_rd == ID_EX_rs2
+                && ~(EX_MEM_RegWrite && EX_MEM_rd != 0 && EX_MEM_rd == ID_EX_rs2))
+            ForwardB = 2'b10;
+        else
+            ForwardB = 2'b00;
     end
 
 endmodule 
