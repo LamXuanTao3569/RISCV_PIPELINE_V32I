e13255438382534bf79215d000e2e1e0ae955216
LamXuanTao3569 <tao.lx203569@sis.hust.edu.vn>
2025-06-24 00:54:05 +0700

    update

--- pipeline_cache/src/Instruction_Memory.v
+++ pipeline_cache/src/Instruction_Memory.v
@@ -12,16 +12,6 @@ module Instruction_Memory(
         end
         $readmemh("mem/imem2.hex", memory);
     end
-    // Task to load instructions (for simulation)
-    task load_instructions;
-        input [31:0] instr_array [0:1023];
-        integer j;
-        begin
-            for (j = 0; j < 1024; j = j + 1) begin
-                memory[j] = instr_array[j];
-            end
-        end
-    endtask
 
     // Asynchronous read with address check
     assign RD = (A[31:2] < 1024) ? memory[A[31:2]] : 32'h00000013;
